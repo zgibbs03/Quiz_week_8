3cc5e9321348b627b6ea5d6bfede595d2d208362
zgibbs03 <zacgibbs32@icloud.com>
2023-09-15 14:30:48 +1000

    week_8 Test Q2
    I selected each individual data columns from the database and fetched all of the results before appending it and adding it to there respective lists

--- climate_q2.py
+++ climate_q2.py
@@ -1,9 +1,23 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+df = pd.read_csv("climate.csv")
+
 years = []
 co2 = []
 temp = []
+
+df_Years = df["Year"]
+for x in df_Years:
+    years.append(x)
+
+df_CO2 = df["CO2"]
+for y in df_CO2:
+    co2.append(y)
+
+df_Temp = df["Temperature"]
+for z in df_Temp:
+    temp.append(z)
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
