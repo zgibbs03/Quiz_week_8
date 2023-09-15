e2594f48872ebded362d58ab4c2cbfb08a14d754
zgibbs03 <zacgibbs32@icloud.com>
2023-09-15 14:09:22 +1000

    week_8 Test Q1
    I selected each individual data columns from the database and fetched all of the results before appending it and adding it to there respective lists

--- climate_q1.py
+++ climate_q1.py
@@ -1,11 +1,29 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+connection = sqlite3.connect("climate.db")
+cursor = connection.cursor()
+
 years = []
 co2 = []
 temp = []
 
+cursor.execute("SELECT Year FROM ClimateData")
+year_Results = cursor.fetchall()
+for x in year_Results:
+    years.append(x)
+
+cursor.execute("SELECT CO2 FROM ClimateData")
+CO2_Results = cursor.fetchall()
+for y in CO2_Results:
+    co2.append(y)
+
+cursor.execute("SELECT Temperature FROM ClimateData")
+temp_Results = cursor.fetchall()
+for z in temp_Results:
+    temp.append(z)
+
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
+plt.plot(years, co2, 'b--')
 plt.title("Climate Data") 
 plt.ylabel("[CO2]") 
 plt.xlabel("Year (decade)") 
